d292e953f8ab5b6f279caeb0ade12cd9c3b5efe3
Casey Williams <caseywilliams@users.noreply.github.com>
2017-10-19 09:17:55 -0700

    (WIP) Fix curl builds on solaris/aix

--- configs/components/curl.rb
+++ configs/components/curl.rb
@@ -17,6 +17,8 @@ component 'curl' do |pkg, settings, platform|
     pkg.environment "PATH" => "/opt/pl-build-tools/bin:$$PATH:#{settings[:bindir]}"
     pkg.environment "PKG_CONFIG_PATH" => "/opt/puppetlabs/puppet/lib/pkgconfig"
     pkg.environment "PATH" => "/opt/pl-build-tools/bin:$$PATH"
+  elsif platform.is_cross_compiled?
+    pkg.environment "PATH", "/opt/pl-build-tools/bin:$(PATH):#{settings[:bindir]}"
   end
 
   if platform.is_windows?
